4506d5c210c7f44d7fea9691e22fe5af00cbca09
bjqrn0 <bjorn.wehlin@gmail.com>
2014-09-02 23:08:07 +0200

    first src commit

---
+++ src/Assets/NetworkManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string gameTypeName = "Keel2";
+	private const string gameName = "TestRoom";
+
+	private HostData[] hostList;
+	
+	public GameObject playerPrefab;
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList (gameTypeName);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+		{
+			hostList = MasterServer.PollHostList();
+		}
+	}
+
+	private void StartServer()
+	{
+		Network.InitializeServer (4, 25000, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (gameTypeName, gameName);
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log ("Server Initialized");
+		
+		SpawnPlayer();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect (hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Connected to Server");
+		
+		SpawnPlayer();
+	}
+
+	private void SpawnPlayer()
+	{
+		Network.Instantiate(playerPrefab, new Vector3(75.0f, 10.0f, 25.0f) , Quaternion.identity, 0);
+	}
+
+	void OnGUI()
+	{
+		if (!Network.isClient && !Network.isServer) {
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+			{
+				StartServer();
+			}
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+			{
+				RefreshHostList();
+			}
+			
+			if (hostList != null)
+			{
+				for (int i = 0; i < hostList.Length; i++)
+				{
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+					{
+						JoinServer(hostList[i]);
+					}
+				}
+			}
+		}
+	}
+	
+}
